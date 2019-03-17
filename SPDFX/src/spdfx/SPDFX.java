1b8279e8c8df7d6993cc79d114d75212237312bf
User <User@10.0.0.39>
2019-03-16 20:37:31 -0400

    Changed header

---
+++ SPDFX/src/spdfx/SPDFX.java
@@ -0,0 +1,21 @@
+package spdfx;
+
+/** 
+ * @Course: SDEV 350 ~ Java Programming III
+ * @Author Name: Group 2
+ * @Assignment Name: spdfx
+ * @Date: Mar 16, 2019
+ * @Description: 
+ */
+
+//Imports
+import javafx.application.Application;
+import javafx.stage.Stage;
+//Begin Class SPDFX
+public class SPDFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+} //End Class SPDFX
